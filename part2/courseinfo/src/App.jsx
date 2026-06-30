c8656de10882c3c1ed1dedcc30ff2864acc5a406
MD . KAMRUL ISLAM <ikamrul483@gmail.com>
2026-06-30 07:07:07 +0600

    courseinfo_2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  console.log(course);
+
+  return <Course course={course} />;
+};
+
+export default App;
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course}></Header>
+      <Content course={course}></Content>
+    </>
+  );
+};
+
+const Header = ({ course }) => {
+  console.log(course);
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => (
+        <Part part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </div>
+  );
+};
